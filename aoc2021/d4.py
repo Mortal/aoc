2c14b411a5a74e03120cd3b38bf862423a40cf01
Mathias Rav <m@git.strova.dk>
2024-10-19 16:43:38 +0100

    d4

--- aoc2021/d4.py
+++ aoc2021/d4.py
@@ -5,4 +5,11 @@ nums = list(map(int, nums_str.split(",")))
 
 boards = []
 for b in boards_str:
-    board = [nums.index(n) for n in list(map(int, b.split()))]
+    rows = [[nums.index(n) for n in list(map(int, line.split()))] for line in b.splitlines()]
+    columns = list(zip(*rows))
+    when = min(*[max(r) for r in rows + columns])
+    unmarked = sum(nums[i] for row in rows for i in row if i > when)
+    score = unmarked * nums[when]
+    boards.append((when, score))
+print(min(boards))
+print(max(boards))
