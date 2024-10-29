954dc27046ecd2dc1a926ae4cf32fe81ccb766df
Mathias Rav <m@git.strova.dk>
2024-10-29 20:43:32 +0100

    d14

---
+++ aoc2021/d14.py
@@ -0,0 +1,17 @@
+from aoc import sectiontoks
+from collections import Counter
+
+tpl = sectiontoks[0][0][0]
+rules = {a:c for a, b, c in sectiontoks[1]}
+state = Counter(zip(tpl, tpl[1:]))
+counts = Counter(tpl)
+for _ in range(40):
+    newstate = Counter()
+    for (a, b), c in state.items():
+        d = rules[a+b]
+        newstate[a,d]+=c
+        newstate[d,b]+=c
+        counts[d] += c
+    state = newstate
+print(counts)
+print(max(counts.values())-min(counts.values()))
