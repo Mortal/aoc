78baeb72514fb81eaced8005da9ea2601d3bc7a7
Mathias Rav <m@git.strova.dk>
2024-12-10 07:44:14 +0100

    Show time also when more than 24h

--- api_to_timestamps.sh
+++ api_to_timestamps.sh
@@ -14,7 +14,15 @@ to_entries[]|
 (if $day < 10 then "0\($day)" else "\($day)" end) as $dayfmt|
 [
 "\($event)d\($dayfmt)",
-(if $totalseconds >= 24*3600 then "    >24h" else $totalseconds|strftime("%H:%M:%S") end),
+(if $totalseconds >= 24*3600 then
+	if $totalseconds >= 100*24*3600 then
+		("\($totalseconds / 3600 / 24|floor)"|"t=" + "0"*(5 - length) + . + "d  ")
+	else
+		("\($totalseconds / 3600|floor)"|"hrs=" + "0"*(3 - length) + . + " ")
+	end
+else
+	$totalseconds|strftime("%H:%M:%S")
+end),
 "star\($star)",
 $name
 ]|@tsv' -cr
