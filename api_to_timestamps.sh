0619d8d6d0b37528772ff4a13569a655b58a9383
Mathias Rav <m@git.strova.dk>
2024-12-10 06:12:26 +0100

    Leading zero for day number

--- api_to_timestamps.sh
+++ api_to_timestamps.sh
@@ -6,13 +6,14 @@ to_entries[]|
 .value.name as $name|
 .value.completion_day_level|
 to_entries[]|
-.key as $day|
+(.key|tonumber) as $day|
 .value|
 to_entries[]|
 .key as $star|
 ((.value.get_star_ts|tonumber)-("\($event)-12-\($day)T05:00:00Z"|fromdateiso8601)) as $totalseconds|
+(if $day < 10 then "0\($day)" else "\($day)" end) as $dayfmt|
 [
-"\($event)d\($day)",
+"\($event)d\($dayfmt)",
 (if $totalseconds >= 24*3600 then "    >24h" else $totalseconds|strftime("%H:%M:%S") end),
 "star\($star)",
 $name
