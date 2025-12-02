24a9e77ed414f5a1f499511c84fca23824d4e334
Mathias Rav <m@git.strova.dk>
2025-12-02 06:08:10 +0100

    2025d2 part 2
    Part 2: 00:08:01

--- aoc2025/day2.py
+++ aoc2025/day2.py
@@ -2,9 +2,20 @@ from aoc import ints
 
 ranges = list(zip(ints[::2],ints[1::2]))
 p1 = 0
+p2 = 0
 for a, b in ranges:
     for i in range(a, b + 1):
-        n = len(str(i))
-        if n % 2 == 0 and str(i)[:n//2] == str(i)[n//2:]:
-            p1 += i
+        s = str(i)
+        n = len(s)
+        for k in range(2, n+1):
+            if n % k:
+                continue
+            m = n//k
+            if all(s[:m] == s[i:i+m] for i in range(0, n, m)):
+                p2 += i
+                if k == 2:
+                    p1 += i
+                # print(a, b, i, s[:m])
+                break
 print(p1)
+print(p2)
