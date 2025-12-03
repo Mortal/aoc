ffd69f681fd90975394b03298172285be31c576e
Mathias Rav <m@git.strova.dk>
2025-12-03 06:13:40 +0100

    2025d3
    Part 2: 00:12:24

--- aoc2025/day3.py
+++ aoc2025/day3.py
@@ -1,7 +1,15 @@
 from aoc import lines
 
 p1 = 0
+p2 = 0
 for line in lines:
-    m = max(int(a + b) for i, a in enumerate(line) for b in line[i+1:])
-    p1 += m
+    maxsofar = []
+    for char in line:
+        maxsofar.append(maxsofar[-1] + char if maxsofar else char)
+        for i in range(len(maxsofar))[::-1]:
+            maxsofar[i] = max(maxsofar[i], f"{maxsofar[i-1]}{char}" if i else char)
+        assert [len(x) for x in maxsofar] == list(range(1, len(maxsofar) + 1)), maxsofar
+    p1 += int(maxsofar[1])
+    p2 += int(maxsofar[11])
 print(p1)
+print(p2)
