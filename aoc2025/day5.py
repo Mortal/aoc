601f536a766459372cd4adafdd3e4b27f3031645
Mathias Rav <m@git.strova.dk>
2025-12-05 06:03:58 +0100

    2025d5
    Part 2: 00:03:55

--- aoc2025/day5.py
+++ aoc2025/day5.py
@@ -3,4 +3,15 @@ from aoc import sectionints
 rangeints, idlines = sectionints
 ids = [i for i, in idlines]
 ranges = [range(a, b+1) for a, b in rangeints]
-print(sum(any(i in r for r in ranges) for i in ids))
+p1 = sum(any(i in r for r in ranges) for i in ids)
+print(p1)
+vs = [(a, 1) for a, b in rangeints] + [(b+1, -1) for a, b in rangeints]
+prev = 0
+thesum = 0
+p2 = 0
+for val, sign in sorted(vs):
+    if thesum:
+        p2 += val - prev
+    thesum += sign
+    prev = val
+print(p2)
