b6e782bbd633b0a4a9e0f61722b876b1729fdd15
Mathias Rav <m@git.strova.dk>
2025-12-06 06:19:44 +0100

    2025d6
    Part 2: 00:19:33

--- aoc2025/day6.py
+++ aoc2025/day6.py
@@ -1,4 +1,4 @@
-from aoc import linetoks
+from aoc import linetoks, lines
 
 p1 = 0
 for col in zip(*linetoks):
@@ -12,3 +12,31 @@ for col in zip(*linetoks):
             m *= i
         p1 += m
 print(p1)
+p2 = 0
+*intlines, oplines = lines
+ops = oplines.split()
+assert all(len(line) == len(intlines[0]) for line in intlines)
+
+buf = []
+res = []
+for col in zip(*intlines):
+    if all(c == " " for c in col):
+        res.append(buf[:])
+        del buf[:]
+    else:
+        buf.append(int("".join(c.strip() for c in col)))
+assert buf
+res.append(buf[:])
+assert len(ops) == len(res)
+for op, myints in zip(ops, res):
+    print(p2, op, myints, myints[0], sum(myints))
+    if op == "+":
+        p2 += sum(myints)
+        print(sum(myints))
+    else:
+        m = 1
+        for i in myints:
+            m *= i
+        print(m)
+        p2 += m
+print(p2)
