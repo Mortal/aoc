7b4a6656da458803c728ab06c13e3e7faa880e67
Mathias Rav <m@git.strova.dk>
2025-12-08 06:08:22 +0100

    2025d8
    Part 1: 00:08:19

---
+++ aoc2025/day8.py
@@ -0,0 +1,33 @@
+from aoc import lineints
+from math import prod
+
+def d(i, j):
+    x1,y1,z1 = lineints[i]
+    x2,y2,z2 = lineints[j]
+    return (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
+
+n = len(lineints)
+dists = sorted((d(i, j), i, j) for i in range(n) for j in range(i+1,n))
+
+rep = list(range(n))
+
+def findrep(i):
+    while rep[i] != rep[rep[i]]:
+        rep[i] = i = rep[rep[i]]
+    return rep[i]
+
+conns = 0
+for dist, i, j in dists:
+    print(dist, i, j, findrep(i), findrep(j), lineints[i], lineints[j])
+    if findrep(i) != findrep(j):
+        rep[findrep(i)] = findrep(j)
+    conns += 1
+    if conns == 1000:
+        break
+sizes = {}
+for i in range(n):
+    r = findrep(i)
+    sizes[r] = sizes.get(r, 0) + 1
+print(sizes)
+print(sum(sizes.values()))
+print(prod(sorted(sizes.values())[-3:]))
