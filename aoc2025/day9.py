f0c6642e3c3463a3eef453d3437202814af5dae1
Mathias Rav <m@git.strova.dk>
2025-12-09 06:55:54 +0100

    2025d9: Clean solution
    	$ time pypy3 day9.py
    	=============> inputs/day9-samp.txt <=============
    	50
    	24
    	=============> inputs/day9-test.txt <=============
    	4715966250
    	1530527040
    
    	real	0m2.251s
    	user	0m2.206s
    	sys	0m0.038s

--- aoc2025/day9.py
+++ aoc2025/day9.py
@@ -1,59 +1,47 @@
 from aoc import lineints
 
-p1, px1, py1, px2, py2 = max(
-    (abs(x1 - x2 + 1) * abs(y1 - y2 + 1), x1, y1, x2, y2)
-    for x1, y1 in lineints
-    for x2, y2 in lineints
+# Part 1:
+print(
+    max(
+        (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+        for x1, y1 in lineints
+        for x2, y2 in lineints
+    )
 )
-print(p1)
 
+# Part 2:
+# Coordinate compression
 xs, ys = zip(*lineints)
 xc = {x: i for i, x in enumerate(sorted(set(xs)))}
 yc = {y: i for i, y in enumerate(sorted(set(ys)))}
-xgrid = [[0] * len(xc) for _ in range(len(yc))]
-ygrid = [[0] * len(xc) for _ in range(len(yc))]
+
+# First, create grid of -1/0/+1 with +1 on +y edges and -1 on -y edges.
+grid = [[0] * len(xc) for _ in range(len(yc))]
 for (x1, y1), (x2, y2) in zip(lineints, lineints[1:] + lineints[:1]):
-    assert x1 == x2 or y1 == y2
-    for x in range(xc[x1], xc[x2]):
-        ygrid[yc[y1]][x] += 1
-    for x in range(xc[x2], xc[x1]):
-        ygrid[yc[y1]][x] -= 1
+    if y1 == y2:
+        continue
+    assert x1 == x2
+    # Note: half-open ranges.
     for y in range(yc[y1], yc[y2]):
-        xgrid[y][xc[x1]] -= 1
+        grid[y][xc[x1]] -= 1
     for y in range(yc[y2], yc[y1]):
-        xgrid[y][xc[x1]] += 1
+        grid[y][xc[x1]] += 1
+
+# Then apply prefix sum to obtain a 0/1 grid with 0=outside, 1=inside
 for y in range(1, len(yc)):
     for x in range(1, len(xc)):
-        xgrid[y][x] += xgrid[y][x - 1]
-        ygrid[y][x] += ygrid[y - 1][x]
-        assert xgrid[y][x] in (0, 1), (x, y, xgrid[y][x])
-        assert ygrid[y][x] in (0, 1), (x, y, ygrid[y][x])
-        assert xgrid[y][x] == ygrid[y][x]
+        grid[y][x] += grid[y][x - 1]
+        assert grid[y][x] in (0, 1)
 
-# for x1, y1 in lineints:
-#    for x2, y2 in lineints:
-#
-
-grid = [
-    ["," if xgrid[y][x] == 1 else "." for x in range(len(xc))] for y in range(len(yc))
-]
-for x, y in lineints:
-    grid[yc[y]][xc[x]] = "#"
-results = sorted(
-    ((abs(x1 - x2) + 1) * (abs(y1 - y2) + 1), x1, y1, x2, y2)
-    for x1, y1 in lineints
-    for x2, y2 in lineints
-    if all(
-        xgrid[y][x] == 1
-        for y in range(yc[min(y1, y2)], yc[max(y1, y2)])
-        for x in range(xc[min(x1, x2)], xc[max(x1, x2)])
+print(
+    max(
+        (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+        for x1, y1 in lineints
+        for x2, y2 in lineints
+        if all(
+            grid[y][x] == 1
+            for y in range(yc[min(y1, y2)], yc[max(y1, y2)])
+            for x in range(xc[min(x1, x2)], xc[max(x1, x2)])
+        )
     )
 )
-for res in results[-10:]:
-    print(*res)
-p2, px1, py1, px2, py2 = results[-1]
-grid[yc[py1]][xc[px1]] = "@"
-grid[yc[py2]][xc[px2]] = "@"
-for row in grid:
-    print("".join(row))
-print(p2)
