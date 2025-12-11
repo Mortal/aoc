759fb01aa93fece62063bd2444c5c108faeb31f4
Mathias Rav <m@git.strova.dk>
2025-12-11 06:06:21 +0100

    2025d11
    Part 2: 00:06:17
    
    I said, have you met our lord and savior @functools.cache?

--- aoc2025/day11.py
+++ aoc2025/day11.py
@@ -5,9 +5,31 @@ edgelists = {}
 for u, *vs in linetoks:
     edgelists[u.strip(":")] = vs
 
-@functools.cache
-def p1count(node: str) -> int:
-    if node == "out":
-        return 1
-    return sum(p1count(v) for v in edgelists[node])
-print(p1count("you"))
+if "you" in edgelists:
+    @functools.cache
+    def p1count(node: str) -> int:
+        if node == "out":
+            return 1
+        return sum(p1count(v) for v in edgelists[node])
+
+    print(p1count("you"))
+
+if "svr" in edgelists:
+    def count(strt: str, goal: str, avoid: str | None, avoid2: str | None):
+        @functools.cache
+        def fn(node: str) -> int:
+            if node == goal:
+                return 1
+            if node == avoid or node == avoid2:
+                return 0
+            return sum(fn(v) for v in edgelists[node])
+
+        return fn(strt)
+
+    svrdac = count("svr", "dac", "fft", "out")
+    dacfft = count("dac", "fft", "svr", "out")
+    fftout = count("fft", "out", "svr", "dac")
+    svrfft = count("svr", "fft", "dac", "out")
+    fftdac = count("fft", "dac", "svr", "out")
+    dacout = count("dac", "out", "svr", "fft")
+    print(svrdac * dacfft * fftout + svrfft * fftdac * dacout)
